2b22ab162070ae7f65c0b7f66c8208a9fd8efccf
Rasmus Bååth <rasmus.baath@gmail.com>
2014-01-26 22:41:30 +0100

    Initial commit

---
+++ R/pingr-package.R
@@ -0,0 +1,25 @@
+#' pingr: The package that sounds like it is called.
+#' 
+#' This package contains one function, ping(), with one purpose: To go ping on
+#' whatever platform you are on. Is intended to be useful, for example, if you
+#' are running a long analysis in the background and want to know when it is
+#' ready. Also useful if you want to irritate colleagues.
+#' 
+#' The package just contains one function \code{\link{ping}}, check it out to
+#' see what it does. For sound on Windows and MacOS \pkg{pingr} depends on the
+#' \pkg{audio} package. For sound on Linux \pkg{pingr} depends on that VLC media
+#' player (http://www.videolan.org/vlc/index.html) is installed and on the PATH.
+#' 
+#' @examples
+#' cat("Countdown: ")
+#' for(i in 10:0) {
+#'   Sys.sleep(0.5)
+#'   cat(i, " ")
+#' }
+#' ping()
+#' 
+#' @name pingr
+#' @docType package
+#' @author Rasmus Bååth < rasmus.baath@@gmail.com >
+#' @import audio stringr
+NULL
---
+++ R/pingr.R
@@ -0,0 +1,91 @@
+#'Play a short sound
+#'
+#'\code{ping} plays a short sound which is useful if you want to get notified, 
+#'for example, when a script has finished. As an added bonus there are a number 
+#'of different sounds to choose from.
+#'
+#'
+#'@param sound character string or number of specifying what sound to be played.
+#'  The default is 1. Possible sounds are: \enumerate{
+#'   \item \code{"ping"}
+#'   \item \code{"coin"}
+#'   \item \code{"fanfare"}
+#'   \item \code{"complete"}
+#'   \item \code{"treasure"}
+#'   \item \code{"ready"}
+#'   \item \code{"shotgun"}
+#'   \item \code{"mario"}
+#'   \item \code{"wilhelm"}
+#' } Any string or number not matching the above sounds will reslut in a random
+#' sound being played.
+#' @param expr An optional expression to be excecuted before the sound.
+#'   
+#'   
+#' @return NULL
+#'   
+#' @examples
+#' cat("Countdown: ")
+#' for(i in 10:0) {
+#'   Sys.sleep(0.5)
+#'   cat(i, " ")
+#' }
+#' ping()
+#' 
+#' # Play a random sound
+#' ping(0)
+#' 
+#' @export
+ping <- function(sound=1, expr=NULL) {
+  expr
+  sounds <- c(ping = "microwave_ping_mono.wav",
+              coin = "smb_coin.wav",
+              fanfare = "victory_fanfare_mono.wav",
+              complete = "work_complete.wav",
+              treasure = "new_item.wav",
+              ready = "ready_master.wav",
+              shotgun = "shotgun.wav",
+              mario = "smb_stage_clear.wav",
+              wilhelm = "wilhelm.wav")
+  sound_fname <- sounds[sound]
+  if(is.na(sound_fname) || length(sound_fname) != 1) {
+    sound_fname <- sample(sounds, size=1)
+  }
+  sound_path <- system.file(paste("sounds/", sound_fname, sep=""), package="pingr")
+  play_file(sound_path)
+}
+
+
+play_vlc <- function(fname) {
+  system(paste("vlc -Idummy --play-and-exit", fname), 
+         ignore.stdout = TRUE, ignore.stderr=TRUE,wait = FALSE)
+  invisible(NULL)
+}
+
+play_audio <- function(fname) {
+  sfx <- load.wave(fname)
+  play(sfx)
+}
+
+play_file <- function(fname) {
+  if(Sys.info()["sysname"] == "Linux" && str_detect(system("which vlc", intern=TRUE), "vlc$")) {
+    play_vlc(fname)
+  } else {
+    play_audio(fname)
+  }
+}
+
+
+# play_vlc_pid <- function(fname) {
+#   system(paste("vlc -Idummy --play-and-exit", fname), 
+#          ignore.stdout = TRUE, ignore.stderr=TRUE,wait = FALSE)
+#   ps_out <- system("ps -eo pid,comm,etime| grep vlc", intern=TRUE)
+#   
+#   # Find the pid
+#   pid_df <- read.table(textConnection(paste(ps_out, collapse="\n")), header=FALSE, 
+#                        stringsAsFactors=FALSE, col.names= c("pid", "command", "time"))
+#   vlc_pid <- pid_df$pid[ # Find the pid that is connected to a vlc
+#     str_detect(pid_df$time, "00:0\\d") & # that is less that 10 seconds old and
+#       which.min(as.numeric(str_extract(pid_df$time, "[0-9]+$"))) # youngest old.
+#     ]
+#   vlc_pid
+# }
