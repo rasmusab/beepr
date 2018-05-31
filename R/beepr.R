f93855a35cc676dd017d1dea8f7143ebe89aba3d
Rasmus Bååth <rasmus.baath@gmail.com>
2018-05-31 23:14:53 +0200

    Release ready for version 1.3

--- R/beepr.R
+++ R/beepr.R
@@ -85,23 +85,81 @@ beep <- function(sound=1, expr=NULL) {
   })
 }
 
+#'Play a short sound if there is an error 
+#'
+#'\code{beep_on_error} wraps an expression and plays a short sound only if an 
+#' error occurs. 
+#'
+#'If \code{beep} is not able to play the sound a warning is issued rather than 
+#'an error. This is in order to not risk aborting or stopping the process that
+#'you wanted to get notified about.
+#'
+#'@param expr An expression to be evaluated for errors.
+#' 
+#'@param sound character string or number specifying what sound to be played by 
+#'  either specifying one of the built in sounds, specifying the path to a wav 
+#'  file or specifying an url. The default is 1. Possible sounds are:
+#'  \enumerate{ \item \code{"ping"} \item \code{"coin"} \item \code{"fanfare"}
+#'  \item \code{"complete"} \item \code{"treasure"} \item \code{"ready"} \item
+#'  \code{"shotgun"} \item \code{"mario"} \item \code{"wilhelm"} \item
+#'  \code{"facebook"} \item \code{"sword"} } If \code{sound} does not match any
+#'  of the sounds above, or is a valid path or url, a random sound will be
+#'  played. Currently \code{beep} can only handle http urls, https is not
+#'  supported.
+#'  
+#'  
+#'@return NULL
+#'  
+#'@examples
+#' # Play a "ping" sound if \code{expr} produces an error
+#' beep_on_error(log("foo"))
+#' 
+#' # Stay silent if \code{expr} does not produce an error
+#' beep_on_error(log(1))
+#' 
+#' \dontrun{
+#' # Play the Wilhelm scream instead of a ping on error.
+#' beep_on_error(runif("bar"), "wilhelm")
+#' }
+#' 
+#'@export
+
+beep_on_error <- function(expr, sound = 1) {
+  q_expr <- substitute(expr)
+  
+  msg <- paste0("An error occurred in ", deparse(q_expr))
+  e <- simpleError(msg)
+  
+  tryCatch(expr, error = function(e) {
+    message(paste0(msg, ": ", e$message))
+    beep(sound)
+  })
+}
+
 is_wav_fname <- function(fname) {
   str_detect(fname, regex("\\.wav$", ignore_case = TRUE))
 }
 
+escape_spaces <- function(s) {
+  str_replace_all(s, " ", "\\\\ ")
+}
+
 play_vlc <- function(fname) {
-  system(paste("vlc -Idummy --no-loop --no-repeat --playlist-autostart --no-media-library --play-and-exit", fname), 
+  fname <- escape_spaces(fname)
+  system(paste0("vlc -Idummy --no-loop --no-repeat --playlist-autostart --no-media-library --play-and-exit ", fname), 
          ignore.stdout = TRUE, ignore.stderr=TRUE,wait = FALSE)
   invisible(NULL)
 }
 
 play_paplay <- function(fname) {
-  system(paste("paplay ", fname), ignore.stdout = TRUE, ignore.stderr=TRUE,wait = FALSE)
+  fname <- escape_spaces(fname)
+  system(paste0("paplay ", fname), ignore.stdout = TRUE, ignore.stderr=TRUE,wait = FALSE)
   invisible(NULL)
 }
 
 play_aplay <- function(fname) {
-  system(paste("aplay --buffer-time=48000 -N -q", fname), ignore.stdout = TRUE, ignore.stderr=TRUE,wait = FALSE)
+  fname <- escape_spaces(fname)
+  system(paste0("aplay --buffer-time=48000 -N -q ", fname), ignore.stdout = TRUE, ignore.stderr=TRUE,wait = FALSE)
   invisible(NULL)
 }
 
